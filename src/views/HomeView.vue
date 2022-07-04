e245402cb1e90f8514d7d11b8104d99480583489
Diego Allies <diegoallies27@gmail.com>
2022-07-04 16:04:25 +0200

    code almost comoplete

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,48 @@
 <template>
 
-<div v-for="units in unit_type" :key="units" >
+<div v-for="units in completeData" :key="units['Booking Unit ID']" >
 
-<div v-for="unit in unit_type2" :key="unit">
+<div class="row">
+  <div class="col-8 uniter">
+
+<h1>
+  Unit Type Info
+</h1> 
+
+<p> Unit Type Id : {{units["Unit Type ID"]}}</p>
+<p> Unit Type Code : {{units["Unit Type Code"]}} </p>
+<p> Unit Type Description : {{units["Unit Type Description"]}} </p>
+<p> Unit Type Category : {{units["Unit Type Category"]}} </p>
+<p> Max Occupants : {{units["Maximum Occupants"]}} </p>
+<p> Max Adults : {{units["Maximum Adults"]}} </p>
+<p> Max Children : {{units["Maximum Children"]}} </p>
+<p> Marketing Description : {{units["Marketing Description"]}} </p>
+<p> Location Id : {{units["Location ID"]}} </p>
+<p> Unit Count : {{units["Unit Count"]}} </p>
+<p> Unit Type Image UIDs : {{units["Unit Type Image UIDs"]}} </p>
+
+
+  </div>
+  <div class="col-4 booker">
+
+<h1>
+  Booking Unit Info
+</h1>
+
+<p> Booking Unit ID : {{units["Booking Unit ID"]}} </p>
+<p> Booking Unit Name : {{units["Booking Unit Name"]}} </p>
+<p> Booking Unit Number : {{units["Booking Unit Number"]}} </p>
+<p> Unit Type ID : {{units["Unit Type ID"]}} </p>
 
 
 
-  {{units}}
-  {{unit}}
+  </div>
 </div>
+
+
+
+
 </div>
-
-
 
 </template>
 <script>
@@ -22,7 +53,7 @@ export default {
   },
   data(){
     return{
-     unit_type:[]
+     completeData: []
     }
   },
   
@@ -43,15 +74,28 @@ axios
     axios.spread((...responses) => {
       const responseOne = responses[0];
       const responseTwo = responses[1];
-  // .then(res => {
-  //     this.payload = res.data
-  //     let bookings= "Booking Unit ID"
-  //     let units= "Unit Types"
-  //     console.log(this.payload["Booking Units", "Unit Types"])
 
+
+      const units = responseOne.data.payload["Unit Types"]    
+      const bookings = responseTwo.data.payload["Booking Units"]
+
+      console.log(units);
+      console.log(bookings);
+
+      this.completeData = bookings.map(booking => {
+        const unitType = units.find(unit => unit["Unit Type ID"] === booking['Unit Type ID'])
+        return {...booking, ...unitType}
+      })
+
+      console.log(completeData)
       // use/access the results
       this.unit_type = responseOne.data.payload["Unit Types"]    
-      this.unit_type2 = responseTwo.data.payload["Booking Units"]    
+      this.unit_type2 = responseTwo.data.payload["Booking Units"]
+      this.unit_type_1_test = responseOne.data.payload["Unit Type ID"]
+      this.unit_type_2_test = responseTwo.data.payload["Unit Type ID"]
+
+      console.log(responseTwo.data.payload["Booking Units"]);
+  
 
     })
   )
@@ -62,168 +106,26 @@ axios
 }
 }
 </script>
+
 <style scoped>
-  /*------------- the rest------------ */
-  #feets{
-    padding-top: 20px;
-  }
-/*------------- Card------------ */
-#col{
-  padding-bottom:20px;
-}
-.card-title{
-  color: #2d2764;
-  font-weight:bold;
-}
-span{
-  width: 10px;
-  height:5px;
-}
-/*------------- Footer------------ */
-footer{
-  background-color: #2d2764;
-  color: rgba(255, 255, 255, 0.923);
-  height:200px;
-  bottom:0;
-}
-/*------------- loader------------ */
-.loader {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  width: 7.33333em;
-  height: 7.33333em;
-  margin-left: -3.66667em;
-  margin-top: -3.66667em;
-}
-.loader-block {
-  position: absolute;
-  top: 0;
-  left: 0;
-  display: inline-block;
-  opacity: 0;
-  width: 2em;
-  height: 2em;
-  background: #2d2764;
-  -webkit-animation: show 0.88s step-end infinite alternate, pulse 0.88s linear infinite alternate;
-  animation: show 0.88s step-end infinite alternate, pulse 0.88s linear infinite alternate;
-}
-.loader-block:nth-child(1) {
-  -moz-transform: translate(0, 0);
-  -ms-transform: translate(0, 0);
-  -webkit-transform: translate(0, 0);
-  transform: translate(0, 0);
-  -webkit-animation-delay: 0.065s;
-  animation-delay: 0.065s;
-}
-.loader-block:nth-child(2) {
-  -moz-transform: translate(2.66667em, 0);
-  -ms-transform: translate(2.66667em, 0);
-  -webkit-transform: translate(2.66667em, 0);
-  transform: translate(2.66667em, 0);
-  -webkit-animation-delay: 0.13s;
-  animation-delay: 0.13s;
-}
-.loader-block:nth-child(3) {
-  -moz-transform: translate(5.33333em, 0);
-  -ms-transform: translate(5.33333em, 0);
-  -webkit-transform: translate(5.33333em, 0);
-  transform: translate(5.33333em, 0);
-  -webkit-animation-delay: 0.195s;
-  animation-delay: 0.195s;
-}
-.loader-block:nth-child(4) {
-  -moz-transform: translate(0, 2.66667em);
-  -ms-transform: translate(0, 2.66667em);
-  -webkit-transform: translate(0, 2.66667em);
-  transform: translate(0, 2.66667em);
-  -webkit-animation-delay: 0.325s;
-  animation-delay: 0.325s;
-}
-.loader-block:nth-child(5) {
-  -moz-transform: translate(2.66667em, 2.66667em);
-  -ms-transform: translate(2.66667em, 2.66667em);
-  -webkit-transform: translate(2.66667em, 2.66667em);
-  transform: translate(2.66667em, 2.66667em);
-  -webkit-animation-delay: 0.13s;
-  animation-delay: 0.13s;
-}
-.loader-block:nth-child(6) {
-  -moz-transform: translate(5.33333em, 2.66667em);
-  -ms-transform: translate(5.33333em, 2.66667em);
-  -webkit-transform: translate(5.33333em, 2.66667em);
-  transform: translate(5.33333em, 2.66667em);
-  -webkit-animation-delay: 0.455s;
-  animation-delay: 0.455s;
-}
-.loader-block:nth-child(7) {
-  -moz-transform: translate(0, 5.33333em);
-  -ms-transform: translate(0, 5.33333em);
-  -webkit-transform: translate(0, 5.33333em);
-  transform: translate(0, 5.33333em);
-  -webkit-animation-delay: 0.39s;
-  animation-delay: 0.39s;
-}
-.loader-block:nth-child(8) {
-  -moz-transform: translate(2.66667em, 5.33333em);
-  -ms-transform: translate(2.66667em, 5.33333em);
-  -webkit-transform: translate(2.66667em, 5.33333em);
-  transform: translate(2.66667em, 5.33333em);
-  -webkit-animation-delay: 0.26s;
-  animation-delay: 0.26s;
-}
-.loader-block:nth-child(9) {
-  -moz-transform: translate(5.33333em, 5.33333em);
-  -ms-transform: translate(5.33333em, 5.33333em);
-  -webkit-transform: translate(5.33333em, 5.33333em);
-  transform: translate(5.33333em, 5.33333em);
+
+.booker {
+  width: 25%;
+  height: 30%;
+  padding: 10px;
+  border: 5px solid gray;
+  margin: 0;
 }
 
-@-webkit-keyframes pulse {
-  from,
-  40% {
-    background: #2d2764;
-  }
-  to {
-    background: #dadada;
-  }
-}
-@-webkit-keyframes show {
-  from, 40% {
-    opacity: 0;
-  }
-  41%, to {
-    opacity: 1;
-  }
-}
-@keyframes pulse {
-  from,
-  40% {
-    background: #2d2764;
-  }
-  to {
-    background: #dadada;  
-  }
-}
-@keyframes show {
-  from, 40% {
-    opacity: 0;
-  }
-  41%, to {
-    opacity: 1;
-  }
-}
-/* Body styling */
-html,
-body {
-  height: 100%;
+.uniter {
+  width: 50%;
+  height: 60%;
+  padding: 10px;
+  border: 5px solid gray;
+  margin-right: 10%;
+  margin-left: 10%;
+  margin-bottom: 20%;
 }
 
-body {
-  background: #999 url('data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4gPHN2ZyB2ZXJzaW9uPSIxLjEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGRlZnM+PHJhZGlhbEdyYWRpZW50IGlkPSJncmFkIiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSIgY3g9ImNpcmNsZSIgY3k9ImZhcnRoZXN0LXNpZGUiIHI9IjEwMCUiPjxzdG9wIG9mZnNldD0iMCUiIHN0b3AtY29sb3I9IiMzMzMzMzMiLz48c3RvcCBvZmZzZXQ9IjEwMCUiIHN0b3AtY29sb3I9IiMxMTExMTEiLz48L3JhZGlhbEdyYWRpZW50PjwvZGVmcz48cmVjdCB4PSIwIiB5PSIwIiB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiBmaWxsPSJ1cmwoI2dyYWQpIiAvPjwvc3ZnPiA=');  
-  background: #999 -moz-radial-gradient(circle farthest-side, #333333, #111111);
-  background: #999 -webkit-radial-gradient(circle farthest-side, #333333, #111111);
-  background: #999 radial-gradient(circle farthest-side, #333333, #111111);
-}
 
-</style>
+</style>>
